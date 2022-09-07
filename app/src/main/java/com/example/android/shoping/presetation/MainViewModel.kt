51ea29ac46854f34ac6c106a147ac04dc72a5492
Семён Голубев <41947359+SemkaHub@users.noreply.github.com>
2022-09-08 01:18:05 +1200

    add presentation layer. add ViewModel

---
+++ app/src/main/java/com/example/android/shoping/presetation/MainViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.android.shoping.presetation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.android.shoping.data.ShopListRepositoryImpl
+import com.example.android.shoping.domain.DeleteShopItemUseCase
+import com.example.android.shoping.domain.EditShopItemUseCase
+import com.example.android.shoping.domain.GetShopListUseCase
+import com.example.android.shoping.domain.ShopItem
+
+class MainViewModel : ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = getShopListUseCase.getShopList()
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+    }
+
+    fun changeEnableState(shopItem: ShopItem) {
+        val newItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(newItem)
+    }
+
+}
