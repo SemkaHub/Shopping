6790cea9dcc4baeaabf11bda3ce8a59902a3e382
Семён Голубев <41947359+SemkaHub@users.noreply.github.com>
2022-09-18 22:17:52 +1200

    added ShopItemViewModel.kt

--- app/src/main/java/com/example/android/shoping/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/example/android/shoping/presentation/ShopItemActivity.kt
@@ -2,11 +2,16 @@ package com.example.android.shoping.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
 import com.example.android.shoping.R
 
 class ShopItemActivity : AppCompatActivity() {
+
+    private lateinit var liveData: ShopItemViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
+
     }
 }
